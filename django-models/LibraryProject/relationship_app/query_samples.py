65b0efb392215d8e273dac7d9974b01457749f56
Theunique-1 <udopeter882@gamil.com>
2025-03-04 05:28:21 +0100

    updated file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,28 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-author_name = "John Doe"
-author = Author.objects.get(name = author_name)
-books_by_author = Book.objects.filter(author = author)
+def get_books_by_author(author_name):
+    """Retrieves books by a given author name."""
+    try:
+        author = Author.objects.get(name=author_name)
+        return Book.objects.filter(author=author)
+    except Author.DoesNotExist:
+        print(f"Author '{author_name}' not found.")
+        return []
 
+def get_books_in_library(library_name):
+    """Retrieves books in a given library."""
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' not found.")
+        return []
 
-library_name = "School Library"
-library = Library.objects.get(name = library_name)
-books_in_library = library.books.all()
-
-
-Librarian_library_name = "School Library"
-Librarian_library = library.objects.get(name = Librarian_library_name)
-librarian = Librarian_library_name
+def get_librarians_in_library(library_name):
+    """Retrieves librarians in a given library."""
+    try:
+        library = Library.objects.get(name=library_name)
+        return Librarian.objects.filter(library=library)
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' not found.")
+        return []
