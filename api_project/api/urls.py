2fd4eadfd929e8d29cf44ed414de9f436a9927bc
Theunique-1 <udopeter882@gamil.com>
2025-03-09 12:09:26 +0100

    added authenication settings in api file

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
@@ -7,5 +8,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('get-token/', obtain_auth_token, name='get-token'),  #Endpoint to retieve authentication tokens
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
@@ -11,3 +12,15 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+def get_permission(self):
+    '''''Instantiate and returns the list of permissions that this views requires. '''''
+    if self.action in ['list','retrieve']: # Getting a list of books or a single book
+        permission_classes = [permissions.AllowAny] # Anyone can access this end point
+    elif self.action == 'create': # creating a new book
+        permission_classes = [permissions.IsAuthenticated] # requiring a valid authentication token
+    else:
+        permission_classes = [permissions.IsAdminUser] #requiring an admin user with a valid token
+    return [permission() for permission in permission_classes]
+
