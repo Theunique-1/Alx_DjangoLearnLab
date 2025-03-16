d7fa079d4dac71154fe56e40e5759285e2ae954b
Theunique-1 <udopeter882@gamil.com>
2025-03-16 23:28:42 +0100

    updated file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,3 +8,11 @@ urlpatterns = [
      path('books/int:Pk/update/',BookUpdateView.as_view(),name='book-update'), # url pattern for updating a book by id
      path('books/int:pk/delete/',BookDeleteView.as_view(),name='book-delete'), # url pattern for deleting a book by id
 ]
+
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+]
