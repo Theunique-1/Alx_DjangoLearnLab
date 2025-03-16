30ef590045179aba3b1bed9871148f09bef8567b
Theunique-1 <udopeter882@gamil.com>
2025-03-16 23:11:02 +0100

    updated urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
-urlpatterns [
-     path('books/', BookListView.as_view(), name ='book-list'), # url pattern for listing books
-     path('books/<int:pk/>', BookDetailView.as_view(), name='book-detail'), # url pattern for viewing a book by id
-     path('books/create/', BookCreateView.as_view(), name='book-create'), # url pattern for creating a book
-     path('books/<int:pk>/update/',BookUpdateView.as_view(), name='book-update'), # url pattern for updating a book by id
-     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # url pattern for deleting a book by id
+urlpatterns = [
+     path('books/',BookListView.as_view(),name ='book-list'), # url pattern for listing books
+     path('books/int:pk/',BookDetailView.as_view(),name='book-detail'), # url pattern for viewing a book by id
+     path('books/create/',BookCreateView.as_view(),name='book-create'), # url pattern for creating a book
+     path('books/int:Pk/update/',BookUpdateView.as_view(),name='book-update'), # url pattern for updating a book by id
+     path('books/int:pk/delete/',BookDeleteView.as_view(),name='book-delete'), # url pattern for deleting a book by id
 ]
