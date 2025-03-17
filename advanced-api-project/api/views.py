979e49c71b44fe4fad74c8133d0d2b8c18f31ed4
Theunique-1 <udopeter882@gamil.com>
2025-03-17 01:24:57 +0100

    updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers, permissions
+from rest_framework import generics, filters, serializers, permissions
 from .models import Book
 from .serializers import BookSerializer
 from datetime import datetime
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
-class BookListView(generics.ListAPIView): # view to list all books
+class BookListView(generics.ListAPIView): # view to list and filter all books
     queryset = Book.objects.all() # Get all book object
-    serializer_class = BookSerializer
+    serializer_class = BookSerializer # BookSerializer to convert Book objects to JSON
     permission_classes = [permissions.AllowAny]  # Allow anyone to view
+    
+    filter_backends = [DjangoFilterBackend] #Use DjangoFilterBackend for filtering
+    filterset_fields = ['title', 'author', 'publication_year'] # Define fields that can be used for filtering
+    search_fields = ['title', 'author_name'] # Define searchable fields
+    ordering_fields = ['title', 'publication_year']
 
 
 class BookDetailView(generics.RetrieveAPIView):  # To show details of a single book
