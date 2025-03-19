b479ccb44b10b68402ded03a3444d7b86de5afe5
Theunique-1 <udopeter882@gamil.com>
2025-03-19 22:13:18 +0100

    Added views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+def register(request):
+    """View for user registration."""
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_view(request):
+    """View for user login."""
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'blog/login.html', {'form': form, 'error': "Invalid username or password"})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def logout_view(request):
+    """View for user logout."""
+    logout(request)
+    return redirect('home')
+
+@login_required
+def profile(request):
+    """View for user profile and editing."""
+    user = get_object_or_404(User, pk=request.user.pk)
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = CustomUserChangeForm(instance=user)
+    return render(request, 'blog/profile.html', {'form': form})
+
